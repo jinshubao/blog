f6f5bf10dae8c0cb87debaa6d1eb7f8ecf873561
jinshubao <jinshubao123@126.com>
2017-05-25 22:51:42 +0800

    update

---
+++ src/views/nav3/Page6.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<el-upload action="https://jsonplaceholder.typicode.com/posts/" list-type="picture-card" multiple="true"
+		:on-preview="preview" :on-remove="handleRemove">
+			<i class="el-icon-plus"></i>
+		</el-upload>
+		<el-dialog v-model="dialogVisible" size="tiny">
+			<img width="100%" :src="dialogImageUrl" alt="">
+		</el-dialog>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			dialogImageUrl: '',
+			dialogVisible: false
+		};
+	},
+	methods: {
+		handleRemove(file, fileList) {
+			console.log(file, fileList);
+		},
+		preview(file) {
+			this.dialogImageUrl = file.url;
+			this.dialogVisible = true;
+		}
+	}
+}
+</script>
